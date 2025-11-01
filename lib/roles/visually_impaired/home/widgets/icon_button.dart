5c700b7f30ba465652873c4507fba832c763c1c0
onetwothird <angelitodecatoriaa@gmail.com>
2025-11-01 14:14:18 +0800

    icon buttons

---
+++ lib/roles/visually_impaired/home/widgets/icon_button.dart
@@ -0,0 +1,59 @@
+// File: lib/roles/visually_impaired/home/widgets/icon_button.dart
+import 'package:flutter/material.dart';
+import 'package:seelai_app/themes/constants.dart';
+
+class CustomIconButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback onPressed;
+  final double size;
+  final bool isDarkMode;
+  final bool isSpecial;
+
+  const CustomIconButton({
+    super.key,
+    required this.icon,
+    required this.onPressed,
+    this.size = 28,
+    required this.isDarkMode,
+    this.isSpecial = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: isDarkMode 
+          ? [
+              BoxShadow(
+                color: (isSpecial ? accent : primary).withOpacity(0.3),
+                blurRadius: 12,
+                offset: Offset(0, 4),
+              ),
+            ]
+          : softShadow,
+        borderRadius: BorderRadius.circular(radiusMedium),
+      ),
+      child: Material(
+        color: isDarkMode 
+          ? (isSpecial ? accent.withOpacity(0.25) : primary.withOpacity(0.2))
+          : primaryLight.withOpacity(0.12),
+        borderRadius: BorderRadius.circular(radiusMedium),
+        child: InkWell(
+          onTap: onPressed,
+          borderRadius: BorderRadius.circular(radiusMedium),
+          splashColor: primary.withOpacity(0.3),
+          child: Container(
+            padding: EdgeInsets.all(spacingMedium),
+            child: Icon(
+              icon,
+              size: size,
+              color: isDarkMode 
+                ? (isSpecial ? accent : primaryLight)
+                : primary,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
