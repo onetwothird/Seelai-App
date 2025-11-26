729421d9834f0c849c64ed032d5c42298715aefc
onetwothird <angelitodecatoriaa@gmail.com>
2025-11-26 10:31:42 +0800

    profile_content

--- lib/roles/caretaker/home/sections/profile_content.dart
+++ lib/roles/caretaker/home/sections/profile_content.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously, deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:seelai_app/themes/constants.dart';
 import 'package:seelai_app/firebase/auth_service.dart';
