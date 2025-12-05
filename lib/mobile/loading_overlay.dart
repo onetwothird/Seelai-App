025ca3d5b3ea648349c50a316d3641dbaec93200
onetwothird <angelitodecatoriaa@gmail.com>
2025-12-06 03:34:43 +0800

    new loading

--- lib/mobile/loading_overlay.dart
+++ lib/mobile/loading_overlay.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
-import 'package:seelai_app/themes/constants.dart';
+import 'package:lottie/lottie.dart';
 
 class LoadingOverlay extends StatefulWidget {
   final String message;
@@ -19,31 +19,23 @@ class LoadingOverlay extends StatefulWidget {
 
 class _LoadingOverlayState extends State<LoadingOverlay>
     with TickerProviderStateMixin {
-  late AnimationController _scaleController;
   late AnimationController _fadeController;
-  late Animation<double> _scaleAnimation;
   late Animation<double> _fadeAnimation;
 
   @override
   void initState() {
     super.initState();
 
-    _scaleController = AnimationController(
-      duration: const Duration(milliseconds: 1000),
-      vsync: this,
-    )..repeat(reverse: true);
-
     _fadeController = AnimationController(
-      duration: const Duration(milliseconds: 300),
+      duration: const Duration(milliseconds: 800),
       vsync: this,
-    );
-
-    _scaleAnimation = Tween<double>(begin: 0.8, end: 1.0).animate(
-      CurvedAnimation(parent: _scaleController, curve: Curves.easeInOut),
     );
 
     _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(parent: _fadeController, curve: Curves.easeOut),
+      CurvedAnimation(
+        parent: _fadeController,
+        curve: Curves.easeInOut,
+      ),
     );
 
     if (widget.isVisible) {
@@ -63,7 +55,6 @@ class _LoadingOverlayState extends State<LoadingOverlay>
 
   @override
   void dispose() {
-    _scaleController.dispose();
     _fadeController.dispose();
     super.dispose();
   }
@@ -77,98 +68,14 @@ class _LoadingOverlayState extends State<LoadingOverlay>
       child: Container(
         color: Colors.black.withOpacity(0.7),
         child: Center(
-          child: ScaleTransition(
-            scale: _scaleAnimation,
-            child: Container(
-              padding: const EdgeInsets.all(32),
-              margin: const EdgeInsets.symmetric(horizontal: 40),
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Colors.white,
-                    Colors.white.withOpacity(0.95),
-                  ],
-                ),
-                borderRadius: BorderRadius.circular(24),
-                boxShadow: [
-                  BoxShadow(
-                    color: primary.withOpacity(0.3),
-                    blurRadius: 30,
-                    spreadRadius: 5,
-                    offset: const Offset(0, 10),
-                  ),
-                  BoxShadow(
-                    color: secondary.withOpacity(0.2),
-                    blurRadius: 50,
-                    spreadRadius: 10,
-                    offset: const Offset(0, 20),
-                  ),
-                ],
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  // Animated dots progress indicator
-                  SizedBox(
-                    height: 40,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: List.generate(3, (index) {
-                        return AnimatedBuilder(
-                          animation: _scaleController,
-                          builder: (context, child) {
-                            final delay = index * 0.3;
-                            final progress = (_scaleController.value + delay) % 1.0;
-                            final scale = 0.5 + (0.5 * (1 - (progress - 0.5).abs() * 2));
-                            
-                            return Container(
-                              margin: const EdgeInsets.symmetric(horizontal: 4),
-                              width: 12,
-                              height: 12,
-                              transform: Matrix4.identity()..scale(scale),
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                gradient: primaryGradient,
-                              ),
-                            );
-                          },
-                        );
-                      }),
-                    ),
-                  ),
-                  
-                  const SizedBox(height: 20),
-                  
-                  // Message text with gradient                                                 
-                  ShaderMask(
-                    shaderCallback: (bounds) => primaryGradient.createShader(bounds),
-                    child: Text(
-                      widget.message,
-                      style: bodyBold.copyWith(
-                        fontSize: 18,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w700,
-                        letterSpacing: 0.5,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  
-                  const SizedBox(height: 8),
-                  
-                  Text(
-                    'Please wait...',
-                    style: caption.copyWith(
-                      fontSize: 14,
-                      color: grey,
-                      fontWeight: FontWeight.w500,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
+          child: SizedBox(
+            width: 400,
+            height: 400,
+            child: Lottie.asset(
+              'assets/icons/Seelai.json',
+              fit: BoxFit.contain,
+              repeat: true,
+              animate: true,
             ),
           ),
         ),
