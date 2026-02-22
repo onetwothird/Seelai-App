d755cb4dec76222661c3a7341024f0215a23cb97
onetwothird <angelitodecatoriaa@gmail.com>
2026-02-22 19:12:33 +0800

    firebase services

--- lib/firebase/firebase_services.dart
+++ lib/firebase/firebase_services.dart
@@ -7,20 +7,20 @@ export 'database_service.dart';
 
 // Specialized services
 export 'activity_logs_service.dart';
-export 'visually_impaired/caretaker_patient_service.dart';
-export 'visually_impaired/emergency_contacts_service.dart';
-export 'visually_impaired/emergency_hotline_service.dart';
-export 'visually_impaired/medical_info_service.dart';
+export 'partially_sighted/caretaker_patient_service.dart';
+export 'partially_sighted/emergency_contacts_service.dart';
+export 'partially_sighted/emergency_hotline_service.dart';
+export 'partially_sighted/medical_info_service.dart';
 export 'admin_service.dart';
 export 'caretaker/assistance_request_service.dart';
 export 'auth_service.dart';
-export 'visually_impaired/user_activity_service.dart'; 
-export 'visually_impaired/text_scan_service.dart'; 
-export 'visually_impaired/object_detection_service.dart';
+export 'partially_sighted/user_activity_service.dart'; 
+export 'partially_sighted/text_scan_service.dart'; 
+export 'partially_sighted/object_detection_service.dart';
 export 'caretaker/communications_service.dart';
-export 'visually_impaired/communications_service.dart';
+export 'partially_sighted/communications_service.dart';
 export 'mswd/mswd_location_tracking_service.dart';
-export 'visually_impaired/face_detection_service.dart';
+export 'partially_sighted/face_detection_service.dart';
 export 'mswd/mswd_call_service.dart';
 /// Usage Example:
 /// 
