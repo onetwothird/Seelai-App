08fdfcb91c6501efaa8c3a0b60464a71c93a01ca
onetwothird <angelitodecatoriaa@gmail.com>
2026-03-15 20:23:22 +0800

    added the flutter_dotenv to make the api not leak in github

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:seelai_app/core/firebase_options.dart';
 import 'package:seelai_app/screens/splash_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
-  
+  await dotenv.load(fileName: ".env");
+
   // Initialize the Firebase
   try {
     await Firebase.initializeApp(
